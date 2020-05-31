42538a82f51d312b6e48e635d255a109f99b2933
ivveta <sweta-89-@mail.ru>
2020-05-31 22:16:14 +0300

    adding route for dish in favorites

--- routes/favorites.js
+++ routes/favorites.js
@@ -74,4 +74,76 @@ favoriteRouter.route('/')
       .catch(err => next(err));
   });
 
+favoriteRouter.route('/favorites/:dishId')
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end(`Get operations is not supported on /favorites/:dishId`);
+  })
+  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorites.findOne({ user: req.user._id })
+      .then(favorites => {
+        if (favorites == null) {
+          // создаем новый
+          Favorites
+            .create({ user: req.user._id, dishes: [req.body] })
+            .then(favorites => {
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'application/json');
+              res.json(favorites);
+            }, err => next(err));
+
+          return;
+        }
+
+        if (!favorites.dishes.includes(req.body)) {
+          favorites.dishes.push(req.body);
+
+          favorites.save()
+            .then(favorites => {
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'application/json');
+              res.json(favorites);
+            }, err => next(err));
+
+          return;
+        }
+
+        res.statusCode = 403;
+        res.end(`Dish with id ${req.body} is already in favorites`);
+      }, err => next(err))
+      .catch(err => next(err));
+  })
+  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
+    res.statusCode = 403;
+    res.end(`Put operations is not supported on /favorites/:dishId`);
+  })
+  .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorites.findOne({ user: req.user._id })
+      .then(favorites => {
+        if (favorites == null) {
+          const err = new Error(`Favorites are empty`);
+          err.status = 404;
+          return next(err);
+        }
+
+        const dishIndex = favorites.dishes.findIndex(req.params.dishId);
+
+        if (dishIndex === -1) {
+          const err = new Error(`Dish ${req.params.dishId} is not on the favorites`);
+          err.status = 404;
+          return next(err);
+        }
+
+        favorites.dishes.splice(dishIndex, 1);
+
+        favorites.save()
+          .then(favorites => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(favorites);
+          }, err => next(err));
+      }, err => next(err))
+      .catch(err => next(err));
+  });
 module.exports = favoriteRouter;
