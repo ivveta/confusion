cb1ea8e94e6253d4b7fa9f5fa9b575eaab1dbd1a
ivveta <31349913+ivveta@users.noreply.github.com>
2020-06-03 21:54:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   secretKey: '34563-25753-12345-65435',
   mongoUrl: 'mongodb://localhost:27017/conFusion',
   facebook: {
-    clientId: '882723265542956',
-    clientSecret: 'ac4a2e037ee2a962fa353694329e3b70',
+    clientId: '',
+    clientSecret: '',
   }
 };
